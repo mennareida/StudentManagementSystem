3bc283a1fcbd5dc70acd30bc2833353b1e9793e4
mennareida <145350784+mennareida@users.noreply.github.com>
2026-05-11 11:51:35 +0300

    Add GraduateStudent class with research title

--- GraduateStudent.java
+++ GraduateStudent.java
@@ -1,1 +1,15 @@
+public class GraduateStudent extends Student {
 
+    private String researchTitle;
+
+    public GraduateStudent(String name, int age, int studentId, String researchTitle) {
+        super(name, age, studentId);
+        this.researchTitle = researchTitle;
+    }
+
+    @Override
+    public void displayInfo() {
+        super.displayInfo();
+        System.out.println("Research Title: " + researchTitle);
+    }
+}
